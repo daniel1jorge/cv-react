964a7047ac46489a40531d14504293705c7c0375
Silva jorge Daniel <daniel1jorge@hotmail.com>
2023-02-27 17:35:11 -0300

    se agrega git de este proyecto.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,6 +4,11 @@ export function Footer() {
         <footer className='w3-container w3-blue w3-center w3-margin-top'>
         <i className="w3-hover-opacity"></i>
         <p>Powered by <a target='_blank' rel="noreferrer" href="https://www.w3schools.com/w3css/default.asp">w3.css</a></p>
+        <br/>
+        <p>Este proyecto <a target='_blank' rel="noreferrer" href="https://github.com/daniel1jorge/cv-react">aqui</a></p>
+
+
+        
       </footer>
     ); 
 }
